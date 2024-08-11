5312b5c8b2e0ab5c38f17f4c9c00470a096f8770
Пол Касуса <65332516+can7wysiwyg@users.noreply.github.com>
2024-08-11 16:29:05 +0200

    updates

--- publicRoutes/MagIssuesRoute.js
+++ publicRoutes/MagIssuesRoute.js
@@ -23,4 +23,46 @@ MagIssuesRoute.get("/magissueroute/show_recent_issue", asyncHandler(async(req, r
 
 
 
+MagIssuesRoute.get("/magissueroute/show_all_issues", asyncHandler(async(req, res, next) => {
+
+
+    try {
+
+        const magIssues = await Magazine.find()
+
+
+    res.json({magIssues})
+    
+        
+    } catch (error) {
+        next(error)
+    }
+
+
+}))
+
+
+MagIssuesRoute.get("/magissueroute/show_issue_single/:id", asyncHandler(async(req, res, next) => {
+
+
+    try {
+
+        const{id} = req.params 
+
+        const singleIssue = await Magazine.findById({_id: id})
+
+
+    res.json({singleIssue})
+    
+        
+    } catch (error) {
+        next(error)
+    }
+
+
+}))
+
+
+
+
 module.exports = MagIssuesRoute
